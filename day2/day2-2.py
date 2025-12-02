7327cd57bf3dbd678f4b72349aa71257d6ef16fb
Herman <herman.lindalen.nodeland@outlook.com>
2025-12-02 11:02:52 +0100

    did day 2 part 2

---
+++ day2/day2-2.py
@@ -0,0 +1,25 @@
+import re
+
+
+ids = []
+
+with open('input2.txt', 'r') as file:
+    for line in file.readlines():
+        ids = line.split(',')
+            
+
+print(ids)
+returnNumber = 0
+for pair in ids:
+    num1 = int(pair.split("-")[0])
+    num2 = int(pair.split("-")[1])
+
+    if num1 > num2:
+        num1, num2 = num2, num1
+
+    rangeList = list(range(num1, num2 + 1))
+    for num in rangeList:
+        if re.fullmatch(r"(.+)\1+", str(num)):
+            returnNumber += int(num)
+        
+print(returnNumber)
