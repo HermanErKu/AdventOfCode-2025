393d25ead84a6932bd809ba0d916bbf0f037ed29
Herman <herman.lindalen.nodeland@aenergi.no>
2025-12-03 10:46:09 +0100

    optimized day3 part1

--- day3/day3-1.py
+++ day3/day3-1.py
@@ -1,17 +1,15 @@
+import itertools
+
 joltageRatings = []
 
-with open('input.txt', 'r') as file:
+with open('input2.txt', 'r') as file:
     for line in file.readlines():
         joltageRatings.append(line.removesuffix('\n'))
 
 returnSum = 0
 for rating in joltageRatings:
-    possibleJoltages = set()
 
-    for i in range(len(rating)):
-        for x in range(i+1, len(rating)):
-            possibleJoltages.add(int(rating[i]+rating[x]))
-    
-    returnSum += int(sorted(list(possibleJoltages))[-1])
+    possibleJoltages = set(itertools.combinations(rating, 2))
+    returnSum += (int(''.join(sorted(list(possibleJoltages))[-1])))
 
 print(returnSum)
